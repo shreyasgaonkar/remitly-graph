39af470778d0d8d47ee1e69843dde1eb385d0bdc
sggaonk <sggaonk@amazon.com>
2020-01-24 22:52:51 +0000

    Update code

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var rawData = $.getJSON('https://f0ud2aqo45.execute-api.us-west-2.amazonaws.com/test', gotData, "jsonp");
+    var rawData = $.getJSON("https://f0ud2aqo45.execute-api.us-west-2.amazonaws.com/test", gotData, "jsonp");
 
-  function gotData(data) {
-    console.log("~~~")
-    datePlot =  (data.date);
-    ratePlot = (data.rate);
-    rateExpressPlot = (data.rateExpress);
+    function gotData(data) {
+        datePlot = data.date;
+        ratePlot = data.rate;
+        rateExpressPlot = data.rateExpress;
 
-    var datePlotArray = [];
-    var ratePlotArray = [];
-    var rateExpressPlotArray = [];
+        var datePlotArray = [];
+        var ratePlotArray = [];
+        var rateExpressPlotArray = [];
 
+        for (var i = 0; i < datePlot.length; i++) {
+            datePlotArray.push(datePlot[i]);
+            ratePlotArray.push(ratePlot[i]);
+            rateExpressPlotArray.push(rateExpressPlot[i]);
+        }
 
-    for(var i=0; i<datePlot.length; i++) {
-      datePlotArray.push(datePlot[i])
-      ratePlotArray.push(ratePlot[i])
-      rateExpressPlotArray.push(rateExpressPlot[i])
+        new Chart(document.getElementById("line-chart"), {
+            type: "line",
+            data: {
+                labels: datePlotArray,
+                datasets: [
+                    {
+                        data: ratePlotArray,
+                        label: "Rate",
+                        borderColor: "#3e95cd",
+                        fill: true,
+                        lineTension: 0.25
+                    }, {
+                        data: rateExpressPlotArray,
+                        label: "Express Rate",
+                        borderColor: "#8e5ea2",
+                        fill: false
+                    }
+                ]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Remitly USD To INR"
+                }
+            }
+        });
     }
-
-    //console.log(datePlotArray);
-    //console.log(ratePlotArray);
-
-    new Chart(document.getElementById("line-chart"), {
-    type: 'line',
-    data: {
-      labels: datePlotArray,
-      datasets: [{
-          data: ratePlotArray,
-          label: "Rate",
-          borderColor: "#3e95cd",
-          fill: true,
-          lineTension: 0.25
-        }, {
-        data: rateExpressPlotArray,
-        label: "Express Rate",
-        borderColor: "#8e5ea2",
-        fill: false
-      }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Remitly USD To INR'
-      }
-    }
-    });
-
-  }
-
 });
