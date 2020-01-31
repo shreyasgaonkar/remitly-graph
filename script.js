83ac3f337ca1afa02e5b3cd86eced1a20e0cd61d
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2020-01-31 23:23:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ $(document).ready(function() {
     function gotData(data) {
         datePlot = data.date;
         ratePlot = data.rate;
-        // rateExpressPlot = data.rateExpress;
 
         var datePlotArray = [];
         var ratePlotArray = [];
-        // var rateExpressPlotArray = [];
+        var rateExpressPlotArray = [];
 
         for (var i = 0; i < datePlot.length; i++) {
             datePlotArray.push(datePlot[i]);
-            ratePlotArray.push(ratePlot[i]);
-            // rateExpressPlotArray.push(rateExpressPlot[i]);
+            ratePlotArray.push((Math.max.apply(null, ratePlot[i])));
+            rateExpressPlotArray.push((Math.min.apply(null, ratePlot[i])));
         }
 
         new Chart(document.getElementById("line-chart"), {
@@ -23,13 +22,13 @@ $(document).ready(function() {
                 labels: datePlotArray,
                 datasets: [
                     {
-                        data: ratePlotArray[0],
+                        data: ratePlotArray,
                         label: "Rate",
                         borderColor: "#3e95cd",
                         fill: true,
                         lineTension: 0.25
                     }, {
-                        data: ratePlotArray[1],
+                        data: rateExpressPlotArray,
                         label: "Express Rate",
                         borderColor: "#8e5ea2",
                         fill: false
